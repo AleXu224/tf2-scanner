76bbd323429477e6a1c8502373f7bc952047a3b7
Squizel <iustin224@gmail.com>
2022-09-14 16:29:52 +0300

    Fix black textures

--- utilities/Texture.cpp
+++ utilities/Texture.cpp
@@ -71,6 +71,7 @@ void Texture::loadTextureContent() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     int format;
     if (channels == 1) format = GL_LUMINANCE;
+    else if (channels == 2) format = GL_LUMINANCE_ALPHA;
     else if (channels == 3) format = GL_RGB;
     else if (channels == 4) format = GL_RGBA;
     glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
